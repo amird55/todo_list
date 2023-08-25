520ed205b5cc8aaeced7b13e849bcc619951c04c
amird55 <amir.dgn@outlook.com>
2023-08-25 11:41:03 +0300

    עבודה ראשונית עם בסיסי נתונים

--- index.js
+++ index.js
@@ -6,8 +6,31 @@ app.use(express.json());
 const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({extended: false}));
 
+var db_M = require('./database');
+global.db_pool = db_M.pool;
+
 app.get("/",(req, res) => {
-    res.send("good morning");
+
+    let q=`INSERT INTO \`categories\` (\`name\`) VALUES ('boker')`;
+
+    db_pool.query(q, function(err, rows, fields){
+
+        if(err)
+        {
+            res.status(500).json({message: err})
+            // throw err;
+        }
+        else
+        {
+            res.status(200).json({fields: fields , rows:rows });
+            // res.status(200).json({message: "Added"});
+            // res.status(200).json(req.crs_data_filtered);
+        }
+
+    });
+
+
+    // res.send("good morning");
 });
 
 
