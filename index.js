ae2295b3930246f39a6f4b7efa909a3eb5d13913
amird55 <amir.dgn@outlook.com>
2023-09-01 12:49:23 +0300

    Ejs

--- index.js
+++ index.js
@@ -6,31 +6,15 @@ app.use(express.json());
 const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({extended: false}));
 
+app.set("view engine", "ejs");
+
 var db_M = require('./database');
 global.db_pool = db_M.pool;
 
 app.get("/",(req, res) => {
 
-    let q=`INSERT INTO \`categories\` (\`name\`) VALUES ('boker')`;
+    res.render("mainPage", {pageTitle:"בוקר טוב"});
 
-    db_pool.query(q, function(err, rows, fields){
-
-        if(err)
-        {
-            res.status(500).json({message: err})
-            // throw err;
-        }
-        else
-        {
-            res.status(200).json({fields: fields , rows:rows });
-            // res.status(200).json({message: "Added"});
-            // res.status(200).json(req.crs_data_filtered);
-        }
-
-    });
-
-
-    // res.send("good morning");
 });
 app.post("/Add",(req, res) => {
     let name=req.body.name;
